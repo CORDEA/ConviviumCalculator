822793aef64615a4d891e130e04c1e921745c059
Yoshihiro Tanaka <contact@cordea.jp>
2016-05-08 18:01:47 +0900

    add windows app

---
+++ windows/ConviviumCalculator/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConviviumCalculator
+{
+    public class MainViewModel
+    {
+        private ObservableCollection<ListItem> listItems = new ObservableCollection<ListItem>();
+        public ObservableCollection<ListItem> ListItems
+        {
+            get
+            {
+                return listItems;
+            }
+        }
+
+        public MainViewModel()
+        {
+        }
+    }
+}
