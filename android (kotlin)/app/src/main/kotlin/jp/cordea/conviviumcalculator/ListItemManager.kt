8e4887861f2245bd61644d9363552493f8748bd8
Yoshihiro Tanaka <contact@cordea.jp>
2016-05-09 08:58:09 +0900

    remove unused

--- android (kotlin)/app/src/main/kotlin/jp/cordea/conviviumcalculator/ListItemManager.kt
+++
@@ -1,9 +0,0 @@
-package jp.cordea.conviviumcalculator
-
-/**
- * Created by CORDEA on 2016/01/02.
- */
-@Deprecated("unused")
-object ListItemManager {
-    var items : Array<ListItem> = arrayOf()
-}
