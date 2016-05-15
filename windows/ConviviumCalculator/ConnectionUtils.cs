1347be2baccbaec6e5e80edbd93072a04281b9de
Yoshihiro Tanaka <contact@cordea.jp>
2016-05-15 19:32:42 +0900

    add util class

---
+++ windows/ConviviumCalculator/ConnectionUtils.cs
@@ -0,0 +1,17 @@
+﻿using SQLite.Net;
+using SQLite.Net.Platform.WinRT;
+using System.IO;
+
+namespace ConviviumCalculator
+{
+    class ConnectionUtils
+    {
+        private const string DB_NAME = "conv.sqlite";
+
+        public  static SQLiteConnection GetSQLiteConnection()
+        {
+            var path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, DB_NAME);
+            return new SQLiteConnection(new SQLitePlatformWinRT(), path);
+        }
+    }
+}
