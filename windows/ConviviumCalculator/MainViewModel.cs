3db0482c8ff991c64ca8bda2247aedc69f3dd949
Yoshihiro Tanaka <contact@cordea.jp>
2016-05-15 19:38:15 +0900

    connect to the database

--- windows/ConviviumCalculator/MainViewModel.cs
+++ windows/ConviviumCalculator/MainViewModel.cs
@@ -1,6 +1,6 @@
-﻿using Realms;
+﻿using SQLite.Net;
+using SQLite.Net.Platform.WinRT;
 using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace ConviviumCalculator
 {
@@ -17,14 +17,31 @@ namespace ConviviumCalculator
 
         public void SaveFlag(string name, bool isSwitch)
         {
-            var realm = Realm.GetInstance();
-            var item = realm.All<ListItem>().Where(i => i.Name == name).First();
-            realm.Write(() => item.IsSwitch = isSwitch);
-            realm.Close();
+            SQLiteConnection conn = ConnectionUtils.GetSQLiteConnection();
+            var item = conn.Table<ListItem>().Where(it => it.Name == name).First();
+            item.IsSwitch = isSwitch;
+            conn.RunInTransaction(() =>
+            {
+                conn.Update(item);
+            });
+            conn.Close();
+        }
+
+        private void GetItemsFromDb(SQLiteConnection conn)
+        {
+            var items = conn.Table<ListItem>();
+            foreach(var item in items)
+            {
+                listItems.Add(item);
+            }
         }
 
         public MainViewModel()
         {
+            var conn = ConnectionUtils.GetSQLiteConnection();
+            conn.CreateTable<ListItem>();
+            GetItemsFromDb(conn);
+            conn.Close();
         }
     }
 }
