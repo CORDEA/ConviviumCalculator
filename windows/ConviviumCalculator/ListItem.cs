f67687212b6827f04e4f34c53d3294ea1700be19
Yoshihiro Tanaka <contact@cordea.jp>
2016-05-15 13:57:02 +0900

    change the listItem to realmObject

--- windows/ConviviumCalculator/ListItem.cs
+++ windows/ConviviumCalculator/ListItem.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Realms;
 
 namespace ConviviumCalculator
 {
-    public class ListItem
+    public class ListItem : RealmObject
     {
         public string Name { get; set; }
         public int Price { get; set; }
         public bool IsSwitch { get; set; }
-
-        public ListItem(string name, int price, bool isSwitch)
-        {
-            Name = name;
-            Price = price;
-            IsSwitch = isSwitch;
-        }
     }
 }
