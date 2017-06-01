bd873b3e52fd4878fd0d671e6ec274e5eb541ff0
Yoshihiro Tanaka <contact@cordea.jp>
2017-06-02 08:23:33 +0900

    update libraries, sdk

--- android (kotlin)/app/src/main/kotlin/jp/cordea/conviviumcalculator/InputDataActivity.kt
+++ android (kotlin)/app/src/main/kotlin/jp/cordea/conviviumcalculator/InputDataActivity.kt
@@ -7,21 +7,22 @@ import android.support.v7.widget.Toolbar
 import android.text.SpannableStringBuilder
 import android.widget.EditText
 import butterknife.bindView
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import io.realm.Realm
-import rx.Observable
-import rx.subscriptions.CompositeSubscription
 
 class InputDataActivity : AppCompatActivity() {
 
     private val toolbar: Toolbar by bindView(R.id.toolbar)
+
     private val editText: EditText by bindView(R.id.edittext)
+
     private val fab: FloatingActionButton by bindView(R.id.fab)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_data)
         setSupportActionBar(toolbar)
-        toolbar.title = resources.getString(R.string.input_data_title)
 
         editText.text = SpannableStringBuilder(objToString())
         fab.setOnClickListener {
@@ -32,12 +33,10 @@ class InputDataActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        if (!compositeSubscription.isUnsubscribed) {
-            compositeSubscription.unsubscribe()
-        }
+        compositeSubscription.clear()
     }
 
-    private val compositeSubscription = CompositeSubscription()
+    private val compositeSubscription = CompositeDisposable()
 
     private fun stringToObj(csv: String) {
         val realm = Realm.getDefaultInstance()
@@ -46,15 +45,17 @@ class InputDataActivity : AppCompatActivity() {
         compositeSubscription.add(
                 Observable
                         .just(csv)
-                        .flatMap { Observable.from(it.split('\n')) }
+                        .flatMap { Observable.fromIterable(it.split('\n')) }
                         .map { it.split(',').toTypedArray() }
                         .filter { it.size > 1 }
                         .map {
-                            val item = realm.createObject(ListItem::class.java, it[0])
-                            item.price = it[1].toInt()
-                            item.switch = if (it.size > 2) it[2].toBoolean() else false
+                            it[1].toIntOrNull()?.let { price ->
+                                val item = realm.createObject(ListItem::class.java, it[0])
+                                item.price = price
+                                item.switch = if (it.size > 2) it[2].toBoolean() else false
+                            }
                         }
-                        .doOnCompleted {
+                        .doOnComplete {
                             realm.commitTransaction()
                             realm.close()
                             finish()
@@ -67,11 +68,10 @@ class InputDataActivity : AppCompatActivity() {
     private fun objToString(): String {
         Realm.getDefaultInstance().let {
             return Observable
-                    .from(it.where(ListItem::class.java).findAll())
+                    .fromIterable(it.where(ListItem::class.java).findAll())
                     .map { "%s,%d,%s".format(it.name, it.price, it.switch.toString()) }
                     .toList()
-                    .toBlocking()
-                    .first()
+                    .blockingGet()
                     .joinToString("\n")
         }
     }
